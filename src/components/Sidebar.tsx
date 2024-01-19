3ab580009bd81aa6517633a940d72de15b29a50b
Isaac Yanez <isaac231467@gmail.com>
2024-01-18 21:26:33 -0600

    added some code for the proji app

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,62 @@
+"use client";
+import React from "react";
+import { Icon } from "@iconify/react";
+import { Toggle } from "@/components/ui/toggle";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+
+export default function Sidebar() {
+  return (
+    <div className="w-[275px] flex flex-col border border-slate-800 items-center space-y-10">
+      <div className=" h-[50px] w-full flex justify-between p-3">
+        <Sheet>
+          <SheetTrigger>
+            <Button variant="outline">
+              <Icon icon="lucide:plus" className="text-black" />
+            </Button>
+          </SheetTrigger>
+          <SheetContent>
+            <SheetHeader>
+              <SheetTitle>Are you absolutely sure?</SheetTitle>
+              <SheetDescription>
+                This action cannot be undone. This will permanently delete your
+                account and remove your data from our servers.
+              </SheetDescription>
+            </SheetHeader>
+          </SheetContent>
+        </Sheet>
+
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+            <Button variant="outline">
+              <Icon icon="lucide:settings" className="text-black" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>Settings</DropdownMenuItem>
+            <DropdownMenuItem>Billing</DropdownMenuItem>
+            <DropdownMenuItem>Team</DropdownMenuItem>
+            <DropdownMenuItem>Subscription</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+    </div>
+  );
+}
